a95b59c55dbbf2fb911d87e7e27b6ed4037c44a9
Martin <m.hein090@gmail.com>
2022-06-20 10:47:16 +0100

    modified delimiter

--- modules/CSVreader.py
+++ modules/CSVreader.py
@@ -14,11 +14,13 @@ class CSVreader:
     @staticmethod
     def read_csv(path):
         with open(path, newline='') as csvfile:
-            file_reader = csv.reader(csvfile, delimiter=' ')
+            file_reader = csv.reader(csvfile, delimiter='`')
             file_rows = [x[0].split(',') for x in file_reader]
             file_data = {}
-            for i in range(0, len(file_rows[0])-1):
+            for i in range(0, len(file_rows[0])):
                 key = file_rows[0][i]
+                if len(key) <= 0:
+                    continue
                 values = []
                 for o in range(1, len(file_rows)):
                     values.append(file_rows[o][i])
